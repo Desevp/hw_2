9d2e295a359a0ede5effa9d3a74b00dfec0672af
Jane Des <desevp@gmail.com>
2018-10-08 10:04:39 +0300

    Refactored

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ function initMonitor(el) {
   let screenInner = el.querySelector('.monitor__screen-inner');
   let scaleTextCont = document.querySelector('.monitor__scale');
   let brightnessTextCont = document.querySelector('.monitor__brightness');
+  let coefWidth = 1;
 
   const nodeState = {
       startPosition: 0,
-      dist: 0,
       isScale: false,
-      scaleFactor: 1.0,
       currScale: 1.0,
       maxZoom: 4.0,
       minZoom: 1,
@@ -29,26 +28,10 @@ function initMonitor(el) {
   scaleTextCont.textContent = nodeState.scaleFactor;
   brightnessTextCont.textContent = nodeState.curentBright * 100;
 
-  let indicatorWidth = document.createElement('div');
-  indicatorWidth.className = 'monitor__screen-indicator';
-  screen.appendChild(indicatorWidth);
-  let coefWidth = 1;
-
+  let indicatorWidthElement = createWidthIndicator();
   changeWidthIndicator();
-
-  function changeWidthIndicator() {
-    var widthScreen = screen.offsetWidth;
-    var widthPicture = screenInner.scrollWidth;
-    coefWidth = widthScreen/(widthPicture * nodeState.currScale);
-    var widthIndicator = widthScreen * coefWidth;
-    indicatorWidth.style.width = `${widthIndicator}px`;
-  }
-
   changePositionIndicator(0);
 
-  function changePositionIndicator(value) {
-    indicatorWidth.style.transform = `translateX(${-value * coefWidth * nodeState.currScale}px)`;
-  }
 
   let gestureArray = [];
   let angleStart = 0;
@@ -57,19 +40,17 @@ function initMonitor(el) {
   screenInner.addEventListener('pointerdown', (event) => {
       screen.style.transition = 'none';
       screen.setPointerCapture(event.pointerId);
+
       gestureArray = [ ...gestureArray, {
         id: event.pointerId,
         startX: event.x,
         prevX: event.x,
         startY: event.y,
-        prevY: event.y,
-        prevTs: Date.now(),
-        startPosition: nodeState.startPosition
+        prevY: event.y
       }];
 
       if (gestureArray.length === 2) {
-        nodeState.dist = distance(gestureArray[0], gestureArray[1]);
-        console.log('два пальца');
+        distanceStart = distance(gestureArray[0], gestureArray[1]);
 
         let t1 = {
           x: gestureArray[0].startX,
@@ -82,19 +63,9 @@ function initMonitor(el) {
         }
 
         angleStart = angle(t1, t2);
-        distanceStart = distance(gestureArray[0], gestureArray[1]);
       }
+      console.log('NEW');
   });
-
-  function distance(p1, p2) {
-    return (Math.sqrt(Math.pow((p1.prevX - p2.prevY), 2) + Math.pow((p1.prevY - p2.prevY), 2)));
-  }
-
-  function angle(p1, p2) {
-    return Math.atan2(p2.y - p1.y, p2.x - p1.x);
-    // return Math.atan2(p2.y - p1.y, p2.x - p1.x)*180/Math.PI;
-
-  }
 
   screen.addEventListener('pointermove', (event) => {
       if (gestureArray.lenght) {
@@ -102,13 +73,11 @@ function initMonitor(el) {
       }
 
       if (gestureArray.length === 1) {
+        // Жест влево-вправо
         const {startX} = gestureArray[0];
         const {x} = event;
-
-
         const dx = x - startX;
         let dif = dx + nodeState.startPosition;
-
 
         if (dif + screenInner.scrollWidth - screen.offsetWidth < 0) {
           screenInner.style.transform = `translateX(${screen.offsetWidth - screenInner.scrollWidth}) scale( ${nodeState.currScale}, ${nodeState.currScale})`
@@ -128,6 +97,7 @@ function initMonitor(el) {
       }
       else {
         if (gestureArray.length === 2) {
+          // Обновляем информацию о прикосновении
           gestureArray.forEach(function(currentGesture){
             if (event.pointerId === currentGesture.id) {
               const {x, y} = event;
@@ -146,32 +116,35 @@ function initMonitor(el) {
             y: gestureArray[1].prevY
           }
 
-
           let tempAngle = Math.abs(angle(newCoord, newCoord2) - angleStart);
           let tempDistance = Math.abs(distance(gestureArray[0], gestureArray[1]) - distanceStart);
 
-
-          if (!nodeState.isScale && ((tempAngle > 0.1) && (tempDistance < 10))) {
-            console.log('rotate');
-            let curAngle = angle(newCoord, newCoord2) - angleStart;
-            nodeState.curentBright = (curAngle > 0)?((nodeState.curentBright >= 1)?1:(nodeState.curentBright * 100 + 2)/100):(nodeState.curentBright <= 0)?0:((nodeState.curentBright * 100 - 2)/100);
-
-            let brightless = nodeState.curentBright;
-
-            screenInner.style.webkitFilter = `brightness(${brightless})`;
-            brightnessTextCont.textContent = nodeState.curentBright * 100;
-          }
-
+          // Pranch
           if (!nodeState.isRotate && ((tempAngle <= 0.1) && (tempDistance > 20))) {
-            console.log('pranch');
-            let currScale = distance(gestureArray[0], gestureArray[1]) / nodeState.dist * nodeState.scaleFactor;
-            nodeState.currScale = (currScale < nodeState.minZoom)?nodeState.minZoom:(currScale > nodeState.maxZoom)?nodeState.maxZoom:currScale;
+            let currScale = distance(gestureArray[0], gestureArray[1]) / distanceStart * nodeState.currScale;
+
+            let tempScale = (currScale < nodeState.minZoom)?nodeState.minZoom:(currScale > nodeState.maxZoom)?nodeState.maxZoom:currScale;
+
             changeWidthIndicator();
-            changePositionIndicator(nodeState.startPosition - indicatorWidth.offsetWidth);
-            nodeState.currFact = currScale;
-            screenInner.style.WebkitTransform = `translateX(${nodeState.startPosition}) scale( ${nodeState.currScale}, ${nodeState.currScale})`;
-            scaleTextCont.textContent = Math.round(nodeState.currScale);
+            changePositionIndicator(nodeState.startPosition - indicatorWidthElement.offsetWidth);
+
+            screenInner.style.WebkitTransform = `translateX(${nodeState.startPosition}) scale( ${tempScale}, ${tempScale})`;
+            nodeState.currScale = tempScale;
+            scaleTextCont.textContent = Math.round(tempScale);
             nodeState.isScale = true;
+          }
+          else {
+            // Rotate
+            if (!nodeState.isScale) {
+              nodeState.isRotate = true;
+              let curAngle = angle(newCoord, newCoord2) - angleStart;
+
+              let brightless = (curAngle > 0)?((nodeState.curentBright >= 1)?1:(nodeState.curentBright * 100 + 2)/100):(nodeState.curentBright <= 0)?0:((nodeState.curentBright * 100 - 2)/100);
+
+              nodeState.curentBright = brightless;
+              screenInner.style.webkitFilter = `brightness(${brightless})`;
+              brightnessTextCont.textContent = Math.round(nodeState.curentBright * 100);
+            }
           }
         }
       }
@@ -192,13 +165,11 @@ function initMonitor(el) {
         nodeState.isRotate = false;
 
         if (nodeState.currScale > nodeState.maxZoom) {
-          nodeState.scaleFactor = maxZoom;
+          nodeState.currScale = maxZoom;
         }
-        else if (nodeState.currScale < nodeState.minZoom) {
-          nodeState.scaleFactor = minZoom;
-        }
-        else {
-          nodeState.scaleFactor = nodeState.currScale;
+
+        if (nodeState.currScale < nodeState.minZoom) {
+          nodeState.currScale = minZoom;
         }
       }
 
@@ -207,4 +178,36 @@ function initMonitor(el) {
 
   screen.addEventListener('pointerup', cancelEvent);
   screen.addEventListener('pointercancel', cancelEvent);
+
+  // Создание индикатора ширины
+  function createWidthIndicator() {
+    let indicatorWidthElement = document.createElement('div');
+    indicatorWidthElement.className = 'monitor__screen-indicator';
+    screen.appendChild(indicatorWidthElement);
+    return indicatorWidthElement;
+  }
+
+  // Изменение ширины индикатора
+  function changeWidthIndicator() {
+    var widthScreen = screen.offsetWidth;
+    var widthPicture = screenInner.scrollWidth;
+    coefWidth = widthScreen/(widthPicture * nodeState.currScale);
+    var widthIndicator = widthScreen * coefWidth;
+    indicatorWidthElement.style.width = `${widthIndicator}px`;
+  }
+
+  // Изменение позиции индикатора
+  function changePositionIndicator(value) {
+    indicatorWidthElement.style.transform = `translateX(${-value * coefWidth * nodeState.currScale}px)`;
+  }
+
+  // Расстояние между двумя точками
+  function distance(p1, p2) {
+    return (Math.sqrt(Math.pow((p1.prevX - p2.prevY), 2) + Math.pow((p1.prevY - p2.prevY), 2)));
+  }
+
+  // Угол между двумя векторами
+  function angle(p1, p2) {
+    return Math.atan2(p2.y - p1.y, p2.x - p1.x);
+  }
 }
